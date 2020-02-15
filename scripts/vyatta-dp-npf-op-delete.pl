1be5879b0befcd803170bcf96b849e3aea891969
ak487r <alexey.khan@intl.att.com>
2020-02-14 20:43:47 -0500

    split <ip:port> to separate attributes in command delete session table
    Fix for VRVDR-47145 broken session table commands
    
    Replace combined filter format for ip and port <ip:[port]> to
    separate ip <ip> and port <port> attributes in yang file to solve
    issue with ipv6:port support.
    
    Updated NPF opd script to support new --flt parameter that accept
    cli parameters source-ip, source-port, destination-ip, destination-port
    and convert it to dataplane filter format to delete matched sessions.

--- scripts/vyatta-dp-npf-op-delete.pl
+++ scripts/vyatta-dp-npf-op-delete.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 
-# Copyright (c) 2019, AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2019-2020, AT&T Intellectual Property. All rights reserved.
 #
 # Copyright (C) 2013-2015 Vyatta, Inc.
 # All rights reserved.
@@ -16,68 +16,142 @@ use Getopt::Long;
 use Vyatta::Dataplane;
 use Vyatta::Dataplane qw(vplane_exec_cmd);
 
-my ( $dp_ids, $dp_conns, $local_controller );
+my %g_yang_to_dataplane_dict = (
+    "source-ip"        => {
+                              "order" => "1",
+                              "dp_name" => "saddr",
+                              "cmd_help" => "[source <src-ip>]",
+                          },
+    "source-port"      => {
+                              "order" => "2",
+                              "dp_name" => "sport",
+                              "cmd_help" => "[source-port <src-port>]",
+                          },
+    "destination-ip"   => {
+                              "order" => "3",
+                              "dp_name" => "daddr",
+                              "cmd_help" => "[destination <dst-ip>]",
+                          },
+    "destination-port" => {
+                              "order" => "4",
+                              "dp_name" => "dport",
+                              "cmd_help" => "[destination-port <dst-port>]",
+                          },
+);
 
 sub usage {
     print "Usage: $0 --id=index\n";
     print "       $0 --all\n";
-    print "       $0 --src=saddr[:sport]\n";
-    print "       $0 --dst=daddr[:dport]\n";
+    print "       $0 --flt <filter_options>\n";
+    print "       <filter_options> = \n";
+
+    foreach my $yang_param ( sort {
+        $g_yang_to_dataplane_dict{$a}{"order"} <=> $g_yang_to_dataplane_dict{$b}{'order'} }
+        keys %g_yang_to_dataplane_dict) {
+        print "            $g_yang_to_dataplane_dict{$yang_param}{'cmd_help'}\n";
+    }
+
     exit 1;
 }
 
-my ( $index, $all, $src, $dst, $fabric );
+# input:
+#     $_[0] - string array of tuples {name_from_yang, value_from_yang}
+#                        {name, value}...{name, value} 
+#     index in array:        0      1        i    i+1   
+#
+# uses:
+#     global hash g_yang_to_dataplane_dict - hash table that contain
+#     yang model valid attributes names as key
+#
+# does: validates format of data coming from yang
+#
+# returns: hash that has <key> set as attribute name and <value> as attribute value.
+sub parse_yang_cmd {
+    my @flt_cli_arr = @_;
+    my %flt_dict;
+    my %dup_params_count;
+
+    if (@flt_cli_arr == 0) {
+        print("Invalid command: --flt <filter_options> - shall contain at least one parameter\n");
+        exit 0;
+    }
+
+    for (my $i = 0; $i < @flt_cli_arr; $i += 2) {
+        my $param = $flt_cli_arr[$i];
+        my $value = $flt_cli_arr[$i+1];
+
+        if ( exists($g_yang_to_dataplane_dict{$param})) {
+            if ( exists($flt_cli_arr[$i+1]) ) {
+                $flt_dict{$param} = $value;
+            } else {
+                print("Invalid command: parameter $param does not have value\n");
+                exit 0;
+            }
+        } else {
+            print("Invalid command: parameter $param is not supported\n");
+            exit 0;
+        }
+
+        if ($dup_params_count{$param}++) {
+            print("Invalid command: duplicate parameter $param\n");
+            exit 0;
+        }
+    }
+
+   return %flt_dict;
+}
+
+# input:   hash that has <key> set as attribute name and <value> as attribute value.
+# does:    converts hash representation of yang data to dataplane string command format.
+# returns: string command line in dataplane format. order of parameters is important
+#          for dataplane
+sub convert_yang_to_dataplane {
+    my %yang_flt_dict = @_;
+    my $dp_filter;
+
+    foreach my $yang_param ( sort { $g_yang_to_dataplane_dict{$a}{"order"}
+        <=> $g_yang_to_dataplane_dict{$b}{'order'}  }
+        keys %g_yang_to_dataplane_dict) {
+        my $dp_param_name;
+        my $dp_param_value;
+
+        if (exists($yang_flt_dict{$yang_param})) {
+            my $yang_value = $yang_flt_dict{$yang_param};
+            $dp_param_value = $yang_value;
+        } else {
+            $dp_param_value = "any";
+        }
+
+        $dp_param_name = $g_yang_to_dataplane_dict{$yang_param}{'dp_name'};
+        $dp_filter .= " $dp_param_name $dp_param_value";
+    }
+
+    return $dp_filter;
+}
+
+my ( $index, $all, @flt, $fabric );
 
 GetOptions(
     'id=s'  => \$index,
     'all'   => \$all,
-    'src=s' => \$src,
-    'dst=s' => \$dst,
+    'flt=s{1,}' => \@flt,
 ) or usage();
+
+my ( $dp_ids, $dp_conns, $local_controller );
 
 ( $dp_ids, $dp_conns, $local_controller ) =
   Vyatta::Dataplane::setup_fabric_conns($fabric);
 
-if ( defined($index) ) {
-    vplane_exec_cmd( "session-op clear session id $index", $dp_ids, $dp_conns, 0 );
-} elsif ( defined($src) || defined($dst) ) {
-    my $filt = "";
-    if ( defined($src) ) {
-        my @ele = split( ":", $src );
-        if ( defined( $ele[0] ) ) {
-            $filt .= "saddr $ele[0] ";
-        } else {
-            $filt .= "saddr any ";
-        }
-
-        if ( defined( $ele[1] ) ) {
-            $filt .= "sport $ele[1] ";
-        } else {
-            $filt .= "sport any ";
-        }
-    } else {
-        $filt .= "saddr any sport any ";
-    }
-
-    if ( defined($dst) ) {
-        my @ele = split( ":", $dst );
-        if ( defined( $ele[0] ) ) {
-            $filt .= "daddr $ele[0] ";
-        } else {
-            $filt .= "daddr any ";
-        }
-
-        if ( defined( $ele[1] ) ) {
-            $filt .= "dport $ele[1] ";
-        } else {
-            $filt .= "dport any ";
-        }
-    } else {
-        $filt .= "daddr any dport any";
-    }
-    vplane_exec_cmd( "session-op clear session filter $filt",
-        $dp_ids, $dp_conns, 0 );
-} elsif ( defined($all) ) {
+if ( defined($all) ) {
     vplane_exec_cmd( "session-op clear session all", $dp_ids, $dp_conns, 0 );
+} elsif ( @flt > 0 ) {
+    my %yang_flt_dict = parse_yang_cmd(@flt);
+    my $dp_flt = convert_yang_to_dataplane(%yang_flt_dict);
+    vplane_exec_cmd( "session-op clear session filter $dp_flt",
+        $dp_ids, $dp_conns, 0 );
+} elsif ( defined($index) ) {
+    vplane_exec_cmd( "session-op clear session id $index", $dp_ids, $dp_conns, 0 );
 }
+
 exit 0;
+
